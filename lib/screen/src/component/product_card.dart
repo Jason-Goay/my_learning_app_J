65515da0a64b5ff135abbe7dc31deb0ab25fc9aa
Jason-Goay <jasongoay1315@gmail.com>
2023-01-05 23:32:16 +0800

    Add home component

--- lib/screen/src/component/product_card.dart
+++ lib/screen/src/component/product_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class ProductCard extends StatefulWidget {
+class ProductCard extends StatelessWidget {
   final String image;
   final String title;
   final String price;
@@ -18,16 +18,48 @@ class ProductCard extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<ProductCard> createState() => _ProductCardState();
-}
-
-// https://github.com/abuanwar072/Flutter-Shop-UI-Kit/blob/master/lib/screens/home/components/product_card.dart
-class _ProductCardState extends State<ProductCard> {
-  @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: widget.onTap,
-      child: Container(),
+      onTap: onTap,
+      child: Container(
+        width: 154,
+        padding: const EdgeInsets.all(10 / 2),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Image.asset(
+                image,
+                height: 132,
+              ),
+            ),
+            const SizedBox(height: 10 / 2),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    style: const TextStyle(color: Colors.black),
+                  ),
+                ),
+                const SizedBox(width: 10 / 4),
+                Text(
+                  "\$" + price.toString(),
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
